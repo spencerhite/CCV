f9f8e426b5125f2f10e75b6d23cab46e777add59
Spencer Hite <44657053+spencerhite@users.noreply.github.com>
2023-05-19 14:40:03 -0400

    Update main.cpp
    Updated isValidCC Funtion and main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <array>
+#include <string>
+#include <algorithm>
 
 using namespace std;
+bool isValidCreditCard(const std::string& creditCardNumber) {
+    std::string number = creditCardNumber;
+    std::reverse(number.begin(), number.end());
+    //uses c++ STL to reverse inputted credit card number
+    int sum = 0;
+    for (size_t i = 0; i < number.length(); ++i) {
+        int digit = number[i] - '0';
 
-bool luhn(const string& nums){
-    int nDigits = nums.size();
-    int nSum = 0, second = false;
-    for(int i = nDigits - 1; i >= 0; i--){
-        int a = nums[i] - '0';
-        if(second == true){
-            a = a*2;
+        if (i % 2 == 1) {
+            digit *= 2; // Double every other digit
+            if (digit > 9)
+                digit -= 9; // Subtract 9 if the result is greater than 9
         }
-        nSum += a / 10;
-        nSum += a % 10;
 
-        second = !second;
-        }
-    return (nSum % 10 == 0);
+        sum += digit;
+    }
+
+    return (sum % 10) == 0; // The card is valid if the sum is divisible by 10
 }
+
 int main() {
+    std::string creditCardNumber;
 
-    string nums = "79927398713";
+    std::cout << "Enter credit card number: ";
+    std::cin >> creditCardNumber;
 
-    if(luhn(nums)){
-        cout<<"This is a valid card"<<endl;
-    }
-    else{
-        cout<<"This is not a valid credit card number"<<endl;
-    }
+    if (isValidCreditCard(creditCardNumber))
+        std::cout << "Valid credit card number." << std::endl;
+    else
+        std::cout << "Invalid credit card number." << std::endl;
 
-
+    return 0;
 }
